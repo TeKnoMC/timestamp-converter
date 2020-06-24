6fc3a821a25c1a8838e7a9143ca1cc9895d2279a
Aiden Stockdale <aidenstockdale@gmail.com>
2020-06-24 18:30:35 +0100

    Make epoch timestamps functional

--- src/converter.js
+++ src/converter.js
@@ -1,45 +1,58 @@
 import React from 'react';
 import { DateTypeEnum, TimeConverter } from './timeconverter'
 
+const TimestampOutput = (props) => {
+    return <p>Output: {props.dateStr}</p>
+}
+
+const TimestampInput = (props) => {
+    return (
+        <form>
+            <label htmlFor="integer">Value:</label>
+            <input 
+                type="number"
+                name="dateValue"
+                id="dateValue"
+                value={props.dateValue}
+                onChange={props.updateMethod}
+            />
+            <label htmlFor="dateType">Timestamp Format:</label>
+            <select name="dateType" id="dateType" onChange={props.updateMethod}>
+                <option value={DateTypeEnum.EPOCH_SECONDS}>Unix Time (Seconds since unix epoch)</option>
+                <option value={DateTypeEnum.EPOCH_MILLISECONDS}>Unix Milliseconds (Milliseconds since unix epoch)</option>
+                <option value={DateTypeEnum.EPOCH_MICROSECONDS}>Unix Microseconds (Microseconds since unix epoch)</option>
+                <option value={DateTypeEnum.CHROME}>Google Chrome Timestamp</option>
+            </select>
+        </form>
+    );
+}
+
 export class Converter extends React.Component {
     state = {
-        dateInteger: 0,
+        dateValue: "0",
         dateType: DateTypeEnum.EPOCH_SECONDS,
         dateStr: ""
     }
 
     updateState = (event) => {
+        // Event data
         const fieldName = event.target.name;
         const value = event.target.value;
 
         let newState = this.state;
 
+        // Add changed state + decode with new data
         newState[fieldName] = value;
-        newState.dateStr = TimeConverter.decode(newState.dateType, newState.dateInteger);
+        newState.dateStr = TimeConverter.decode(newState.dateType, parseInt(newState.dateValue));
 
         this.setState(newState);
     }
 
     render() {
         return (
-            <div>
-                 <form>
-                    <label htmlFor="integer">Value:</label>
-                    <input 
-                        type="number"
-                        name="dateInteger"
-                        id="dateInteger"
-                        value={this.state.dateInteger}
-                        onChange={this.updateState}
-                    />
-                    <select name="dateType" id="dateType" onChange={this.updateState}>
-                        <option value={DateTypeEnum.EPOCH_SECONDS}>Unix Time (Seconds since unix epoch)</option>
-                        <option value={DateTypeEnum.EPOCH_MILLISECONDS}>Unix Milliseconds (Milliseconds since unix epoch)</option>
-                        <option value={DateTypeEnum.EPOCH_MICROSECONDS}>Unix Microseconds (Microseconds since unix epoch)</option>
-                        <option value={DateTypeEnum.CHROME}>Google Chrome Timestamp</option>
-                    </select>
-                </form>
-            <p>{this.state.dateStr}</p>
+            <div class="window">
+                <TimestampInput dateValue={this.state.dateValue} updateMethod={this.updateState}/>
+                <TimestampOutput dateStr={this.state.dateStr} />
             </div>
         );
     }
--- src/timeconverter.js
+++ src/timeconverter.js
@@ -6,19 +6,35 @@ export const DateTypeEnum = {
 };
 
 export class TimeConverter {
-    // Have specific conversion methods
-    // + General method with switch on DateTypeEnum supplied
+    static epochSecondsDecode(value) {
+        return new Date(value*1000).toString();
+    }
+
+    static epochMillisecondsDecode(value) {
+        return new Date(value).toString();
+    }
+
+    static epochMicrosecondsDecode(value) {
+        return new Date(value/1000).toString();
+    }
+
+    static chromeDecode(value) {
+        
+    }
 
     static decode(dateType, value) {
         switch (dateType) {
             case DateTypeEnum.EPOCH_SECONDS:
-                return `Epoch seconds ${value}`;
+                return this.epochSecondsDecode(value);
             case DateTypeEnum.EPOCH_MILLISECONDS:
-                return `Epoch milliseconds ${value}`;
+                return this.epochMillisecondsDecode(value);
             case DateTypeEnum.EPOCH_MICROSECONDS:
-                return `Epoch microseconds ${value}`;
+                return this.epochMicrosecondsDecode(value);
             case DateTypeEnum.CHROME:
                 return `Chrome date ${value}`;
+            default:
+                console.log("Error in timeconverter.js: unknown date type selected");
+                break;
         }
     }
 }
